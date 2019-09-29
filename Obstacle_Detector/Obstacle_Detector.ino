363f1b6c53f357efad9b5e5b0d09ea921c36ebe2
swikriti04 <swikritikhadke@gmail.com>
2019-09-29 06:25:30 +0530

    Create Obstacle_Detector.ino

---
+++ Obstacle_Detector/Obstacle_Detector.ino
@@ -0,0 +1,147 @@
+
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#define TRIGGER_PIN  2
+#define ECHO_PIN     0
+
+WiFiClient client;
+ESP8266WebServer server(80);
+
+String MakerIFTTT_Key;
+String MakerIFTTT_Event;
+char *append_str(char *here, String s) { int i = 0; while (*here++ = s[i]) { i++; }; return here - 1; }
+char *append_ul(char *here, unsigned long u) { char buf[20]; return append_str(here, ultoa(u, buf, 10)); }
+char post_rqst[256]; 
+char *p; 
+char *content_length_here; 
+char *json_start; int compi;
+
+String makerKey = "bTybN0oWUtQWIi4RYu06fouxVNelwLwM7brXXi8X6q";
+String makerEventName = "blind_guy";
+bool isConnected = false;
+
+char* ssid = "HackSRM-2019";
+char* pwd = "TFest#2019";
+String sms = "00919581570571";
+char* smsMessage = "Beware there is an obstacle ahead.";
+
+const int wifiConnectTimeout = 40;
+const int MaxReadings = 20;
+
+
+void setup()
+{
+  Serial.begin (9600);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);    
+
+	WiFi.disconnect();
+	delay(3000);
+	Serial.println(F("START"));
+
+	ConnectToNetwork(ssid, pwd);
+
+}
+
+
+bool ConnectToNetwork(char* ssidName, char* Password)
+{	
+	isConnected = false;
+	
+	WiFi.begin(ssidName, Password);
+	
+	Serial.println("trying to connect");
+	
+	int connectTries = 0;
+
+	while (!(WiFi.status() == WL_CONNECTED))
+	{		
+		Serial.print("#");
+		delay(1000);
+		
+		connectTries += 1;
+
+		if (connectTries == wifiConnectTimeout)
+		{
+			break;
+		}
+	}
+
+	Serial.println("");
+
+	Serial.println(WiFi.status() == 3 ? "Connected to WiFi network." : "Could not connect to WiFi.");
+	
+	if (WiFi.status() == 3)
+	{
+		isConnected = true;
+
+		SendSMS("Connected to WIFI network");
+	}
+	else
+	{
+		isConnected = false;
+	}
+
+	Serial.println(WiFi.localIP());
+	
+	return isConnected;
+}
+
+void loop()
+{
+  
+  long duration, distance;
+  digitalWrite(TRIGGER_PIN, LOW);  
+  delayMicroseconds(2); 
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(1000);
+
+  if (distance<= 100)
+   delay(20);
+}
+
+void SendSMS(char* Message)
+{	
+	if (client.connect("maker.ifttt.com", 80))
+	{
+		MakerIFTTT_Key = makerKey;
+		MakerIFTTT_Event = makerEventName;
+		p = post_rqst;
+		p = append_str(p, "POST /trigger/");
+		p = append_str(p, MakerIFTTT_Event);
+		p = append_str(p, "/with/key/");
+		p = append_str(p, MakerIFTTT_Key);
+		p = append_str(p, " HTTP/1.1\r\n");
+		p = append_str(p, "Host: maker.ifttt.com\r\n");
+		p = append_str(p, "Content-Type: application/json\r\n");
+		p = append_str(p, "Content-Length: ");
+		content_length_here = p;
+		p = append_str(p, "NN\r\n");
+		p = append_str(p, "\r\n");
+		json_start = p;
+		p = append_str(p, "{\"value1\":\"");
+		p = append_str(p, sms);
+		p = append_str(p, "\",\"value2\":\"");
+		p = append_str(p, "");
+		p = append_str(p, "\",\"value3\":\"");
+		p = append_str(p, Message);
+		p = append_str(p, "\"}");
+
+		compi = strlen(json_start);
+		content_length_here[0] = '0' + (compi / 10);
+		content_length_here[1] = '0' + (compi % 10);
+		client.print(post_rqst);
+		client.flush();
+		Serial.println("Sent...");		
+		p = "";
+	}
+}
